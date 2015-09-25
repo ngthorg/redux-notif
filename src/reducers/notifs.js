c986da90d4e2c999977f843afb1fb72a23d6baec
ngthong <ngthorg@gmail.com>
2015-09-25 13:18:08 +0700

    add immutable & update v0.0.6

--- src/reducers/notifs.js
+++ src/reducers/notifs.js
@@ -1,19 +1,21 @@
 import { NOTIF_SEND, NOTIF_DISMISS, NOTIF_CLEAR } from '../actions/notifs'
+import { Map, List } from 'immutable';
 
-export default function notifs(domain = [], action) {
+
+export default function notifs(domain = new List([]), action) {
   if (!action || !action.type) return domain
 
   switch (action.type) {
     case NOTIF_SEND:
-      return [action.payload, ...domain]
+      return domain.concat(Map(action.payload))
 
     case NOTIF_DISMISS:
       return domain.filter(n =>
-          n.id !== action.payload
+        n.get('id') !== action.payload
       )
 
     case NOTIF_CLEAR:
-        return notifClear(domain, action)
+      return notifClear(domain, action)
 
     default:
         return []
